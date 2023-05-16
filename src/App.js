6f10becc3adfe96b319c8cc827badf3fc811e4f2
amshepard <antoineshepard@pursuit.org>
2023-05-16 17:56:07 -0400

    functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
 /*
   Components
@@ -26,9 +27,20 @@ function App() {
   return (
     <div className="wrapper">
       <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home employees={employees} owners={owners} pets={pets} />} />
+        <Route
+          path="/staff"
+          element={<StaffList employees={employees} />} />
+        <Route
+          path="/pets"
+          element={<PetsList pets={pets} />} />
+        <Route
+          path="/pets/:type"
+          element={<PetsList pets={pets}/>} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,8 +1,21 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
 
 export const PetsList = ({ pets }) => {
+
+  const navigate = useNavigate();
+  const { type } = useParams();
+  console.log(type)
+
+  useEffect(() => {
+    if (!type) {
+      navigate("/pets/cats")
+    }
+  }, [])
+
   const [cats, dogs] = pets.reduce(
     (acc, pet) => {
       const position = pet.kind === "Cat" ? 0 : 1;
@@ -12,19 +25,22 @@ export const PetsList = ({ pets }) => {
     [[], []]
   );
 
+
   return (
     <section className="pets-wrapper">
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
         {/* All cats section */}
-        {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
 
+        {type === "cats" ? cats.map((cat) => (
+          <Pet key={cat.id} kind="cat" pet={cat} />
+        ))
+          : null}
         {/* All dogs section */}
-        {dogs.map((dog) => (
+
+        {type === "dogs" ? dogs.map((dog) => (
           <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+        )) : null}
       </section>
     </section>
   );
